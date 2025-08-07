363ceac6a10f6e51ce1007700edc03ae4ad78ae0
Pavel Dumbrao <paveldumbrao90@gmail.com>
2025-08-07 16:28:23 +0300

    index.js

--- deno/index.js
+++ deno/index.js
@@ -1,6 +1,5 @@
 import { Application, Status } from "@oak/oak";
-
-import { corsHeaders, host, port } from "./config.js";
+import { corsHeaders } from "./config.js"; // удаляем host, port из импорта
 import mainRouter from "./routes/index.js";
 
 const app = new Application();
@@ -10,16 +9,17 @@ app.use(async (ctx, next) => {
   for (const corsHeaderKey of Object.keys(corsHeaders)) {
     ctx.response.headers.set(corsHeaderKey, corsHeaders[corsHeaderKey]);
   }
-
   if (ctx.request.method === "OPTIONS") {
     ctx.response.status = Status.NoContent;
     return;
   }
-
   await next();
 });
 
 app.use(mainRouter.routes());
 
-console.log(`🐿️ Oak is running at ${host}:${port}`);
-await app.listen({ host, port });
+// Главная фишка — получение порта из ENV:
+const PORT = Deno.env.get("PORT") || "8080";
+// Можно явно указать, что слушаем на всех интерфейсах:
+console.log(`🐿️ Oak is running at 0.0.0.0:${PORT}`);
+await app.listen({ hostname: "0.0.0.0", port: Number(PORT) });
